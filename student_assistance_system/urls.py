ec2570bfeff64d4be5fc3b59f868136157256ded
Eric Rinehart <edr27@case.edu>
2016-10-21 11:24:29 -0400

    explicit auth url definitions

--- student_assistance_system/urls.py
+++ student_assistance_system/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+import django.contrib.auth.views as auth_views
 
 from . import views
 
@@ -6,5 +7,6 @@ app_name = 'student_assistance_system'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^accounts/profile/', views.profile, name='profile'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/login/', auth_views.login, name='login'),
+    url(r'^accounts/logout/', auth_views.logout, name='logout'),
 ]
