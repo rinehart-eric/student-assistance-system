561db7a008accb0845e40f4f9aab42fce3f8e982
Eric Rinehart <edr27@case.edu>
2016-10-24 19:10:24 -0400

    tests for index view

--- student_assistance_system/tests.py
+++ student_assistance_system/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+test_username = 'test'
+test_password = 'test'
+
+class IndexTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(test_username, password=test_password)
+
+    def test_index_unauthorized(self):
+        url = reverse("student_assistance_system:index")
+        resp = self.client.get(url, follow=True)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIsNotNone(resp.templates)
+        self.assertEqual(resp.templates[0].name, 'registration/login.html')
+
+    def test_index_authorized(self):
+        self.assertTrue(self.client.login(username=test_username, password=test_password))
+
+        url = reverse("student_assistance_system:index")
+        resp = self.client.get(url, follow=True)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIsNotNone(resp.templates)
+        self.assertEqual(resp.templates[0].name, 'student_assistance_system/index.html')
+
+        self.client.logout()
