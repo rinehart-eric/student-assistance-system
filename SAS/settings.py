6487d297694540e675e1c8016abd1b08d89d5cc9
Eric Rinehart <edr27@case.edu>
2016-10-24 19:31:21 -0400

    changed UT runner

--- SAS/settings.py
+++ SAS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,16 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Unit Testing
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=student_assistance_system',
+    '--cover-html',
+]
 
 
 # Static files (CSS, JavaScript, Images)
