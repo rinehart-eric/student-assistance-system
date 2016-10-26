728bbffd42031711a2d11238c9b223efda4ace26
Eric Rinehart <edr27@case.edu>
2016-10-26 16:59:16 -0400

    added schedule view and cleaned up schedule inclusion tag

--- student_assistance_system/templatetags/schedule_view.py
+++ student_assistance_system/templatetags/schedule_view.py
@@ -4,10 +4,6 @@ from student_assistance_system.models import Schedule
 register = template.Library()
 
 
-@register.inclusion_tag('student_assistance_system/schedule_view.html')
+@register.inclusion_tag('student_assistance_system/fragments/schedule_view.html')
 def schedule_view(schedule):
-    """
-    :type schedule: Schedule
-    """
-    assert isinstance(schedule, Schedule)
     return dict(schedule=schedule)
