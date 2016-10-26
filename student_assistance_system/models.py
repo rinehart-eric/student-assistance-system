99bcc564a73691f97fa540e494a1175a935c676f
Eric Rinehart <edr27@case.edu>
2016-10-26 15:45:06 -0400

    modified our user model to extend django user model

--- student_assistance_system/models.py
+++ student_assistance_system/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 
 class Department(models.Model):
@@ -33,9 +36,21 @@ class RequirementSet(models.Model):
     requirements = models.ManyToManyField(Requirement)
 
 
-class User(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     minors = models.ManyToManyField(RequirementSet)
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Section(models.Model):
@@ -56,17 +71,18 @@ class MeetingTime(models.Model):
 class Schedule(models.Model):
     name = models.CharField(max_length=50)
     sections = models.ManyToManyField(Section)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(Profile)
+    updated = models.DateTimeField(auto_now=True)
 
 
 class CompletedCourse(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(Profile)
     course = models.ForeignKey(Course)
     grade = models.CharField(max_length=1)
 
 
 class UserMajor(models.Model):
-    name = models.ForeignKey(User)
+    name = models.ForeignKey(Profile)
     major = models.ForeignKey(RequirementSet)
     concentration = models.ForeignKey(RequirementSet, related_name='+')
 
