3ca2e3fbd8523bf9f7a630b04b6b03e0e0c8152f
Eric Rinehart <edr27@case.edu>
2016-11-11 22:23:27 -0500

    Switched to class-based views

--- student_assistance_system/urls.py
+++ student_assistance_system/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 import django.contrib.auth.views as auth_views
 
-from . import views
+from views import *
 
 app_name = 'student_assistance_system'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^schedules/(?P<schedule_id>[0-9]+)/$', views.view_schedule, name='view_schedule'),
-    url(r'^schedules/(?P<schedule_id>[0-9]+)/edit/$', views.edit_schedule, name='edit_schedule'),
-    url(r'^accounts/profile/$', views.profile, name='profile'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^schedules/(?P<schedule_id>[0-9]+)/$', ViewScheduleView.as_view(), name='view_schedule'),
+    url(r'^schedules/(?P<schedule_id>[0-9]+)/edit/$', EditScheduleView.as_view(), name='edit_schedule'),
+    url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
 ]
--- student_assistance_system/views.py
+++ student_assistance_system/views.py
@@ -1,32 +1,53 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
 
 
-@login_required
-def index(request):
-    schedules = request.user.profile.schedule_set.all()
-    most_recently_updated_schedule = max(schedules, key=lambda s: s.updated) if schedules else None
-    return render(request, 'student_assistance_system/index.html', dict(schedule=most_recently_updated_schedule))
+@method_decorator(login_required, name='dispatch')
+class IndexView(View):
+    template_name = 'student_assistance_system/index.html'
+
+    def get_requirement_sets(self, p):
+        majors_and_concentrations = [(um.major, um.concentration) for um in p.usermajor_set.all()]
+        return list(p.minors.all()) + list(sum(majors_and_concentrations, ()))
+
+    def get(self, request, *args, **kwargs):
+        p = request.user.profile
+        schedules = request.user.profile.schedule_set.all()
+        req_sets = self.get_requirement_sets(p)
+        most_recently_updated_schedule = max(schedules, key=lambda s: s.updated) if schedules else None
+
+        return render(request, self.template_name, dict(schedule=most_recently_updated_schedule, req_sets=req_sets))
 
 
-@login_required
-def view_schedule(request, schedule_id):
-    profile = request.user.profile
-    schedule = profile.schedule_set.filter(pk=schedule_id).first()
+@method_decorator(login_required, name='dispatch')
+class ViewScheduleView(IndexView):
+    template_name = 'student_assistance_system/view_schedule.html'
 
-    majors_and_concentrations = [(um.major, um.concentration) for um in profile.usermajor_set.all()]
-    req_sets = list(profile.minors.all()) + list(sum(majors_and_concentrations, ()))
+    def get(self, request, *args, **kwargs):
+        p = request.user.profile
+        schedule = p.schedule_set.filter(pk=self.kwargs['schedule_id']).first()
+        req_sets = self.get_requirement_sets(p)
 
-    return render(request, 'student_assistance_system/view_schedule.html', dict(schedule=schedule, req_sets=req_sets))
-
-
-@login_required
-def edit_schedule(request, schedule_id):
-    schedule = request.user.profile.schedule_set.filter(pk=schedule_id).first()
-    # TODO: create this template
-    return render(request, 'student_assistance_system/edit_schedule.html', dict(schedule=schedule))
+        return render(request, self.template_name, dict(schedule=schedule, req_sets=req_sets))
 
 
-@login_required
-def profile(request):
-    return render(request, 'student_assistance_system/profile.html', dict(user=request.user))
+@method_decorator(login_required, name='dispatch')
+class EditScheduleView(View):
+    template_name = 'student_assistance_system/edit_schedule.html'
+
+    def get(self, request, *args, **kwargs):
+        p = request.user.profile
+        schedule = p.schedule_set.filter(pk=self.kwargs['schedule_id']).first()
+
+        # TODO: create this template
+        return render(request, self.template_name, dict(schedule=schedule))
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    template_name = 'student_assistance_system/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, dict(user=request.user))
