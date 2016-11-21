bc6da1c0f4e8d8c91b0f58b9c45b27e404f772a3
relytred <tmayle@patriotsoftware.com>
2016-11-21 01:32:06 -0500

    Section view url

--- student_assistance_system/urls.py
+++ student_assistance_system/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     url(r'^schedules/(?P<schedule_id>[0-9]+)/$', ViewScheduleView.as_view(), {'editing': False}, name='view_schedule'),
     url(r'^schedules/(?P<schedule_id>[0-9]+)/edit/$', ViewScheduleView.as_view(), {'editing': True}, name='edit_schedule'),
     url(r'^schedules/(?P<schedule_id>[0-9]+)/edit/(?P<section_id>[0-9]+)/remove/$', RemoveSectionScheduleView.as_view(), {'editing': True}, name='remove_section'),
+    url(r'^sections/(?P<section_id>[0-9]+)/$', ViewSectionView.as_view(), name='view_section'),
     url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
-    url(r'^search', SearchView.as_view()),
+    url(r'^search', SearchView.as_view(), name='search'),
     url(r'^results', SearchResultsView.as_view(), name='courses'),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout')
