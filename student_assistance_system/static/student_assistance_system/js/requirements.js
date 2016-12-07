01fcd56a2d95b92db4729d4ede45ed7526090d3f
Eric Rinehart <edr27@case.edu>
2016-12-07 11:35:54 -0500

    Revert "Toggle course number"
    This reverts commit 2d741c814e82f12569d8c3808ea004b5988834b5.

--- student_assistance_system/static/student_assistance_system/js/requirements.js
+++ student_assistance_system/static/student_assistance_system/js/requirements.js
@@ -2,15 +2,5 @@ function showHideDiv(elemId) {
     var div = document.getElementById(elemId)
     div.style.display = div.style.display == "block" ? "none" : "block";
 }
-function showHideInlineElement(elemId, buttonClicked) {
-    var div = document.getElementById(elemId)
-    var buttonElement = document.getElementById(buttonClicked)
-    console.log(buttonClicked)
-    console.log(buttonElement)
-    div.style.display = div.style.display == "inline" ? "none" : "inline";
-    console.log(buttonElement.style.display)
-    buttonElement.style.display = buttonElement.style.display == "inline-flex" ? "none" : "inline-flex";
-    console.log(buttonElement)
-}
 
 
